1d7d43460d0aeab750e3ab84307c8dea43ed7800
merey <merey.t.08@mail.ru>
2026-01-25 11:20:25 +0500

    deleted coomments

--- src/main/java/taskmanager/entities/User.java
+++ src/main/java/taskmanager/entities/User.java
@@ -13,13 +13,13 @@ public class User {
         this.role = role;
     }
 
-    // ✅ Getters
+
     public int getId() { return id; }
     public String getName() { return name; }
     public String getEmail() { return email; }
     public String getRole() { return role; }
 
-    // ✅ Setters
+
     public void setId(int id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
