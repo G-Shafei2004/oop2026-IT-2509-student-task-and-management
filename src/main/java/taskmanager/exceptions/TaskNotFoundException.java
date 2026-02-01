553f4227d1772a3f410672172d6b2dcf1824f1ae
Ghulam Ali Shafei <ghulamalishafei2023@gmail.com>
2026-02-01 13:28:02 +0430

    Ghulam Ali Shafei-add Builder pattern for Task entity

--- src/main/java/taskmanager/exceptions/TaskNotFoundException.java
+++ src/main/java/taskmanager/exceptions/TaskNotFoundException.java
@@ -2,6 +2,6 @@ package taskmanager.exceptions;
 
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(String message) {
-        super(message);
+        super(  );
     }
 }
