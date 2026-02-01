c81c60ddce06d596323215e40abbf56a2d670c49
merey <merey.t.08@mail.ru>
2026-02-02 01:04:58 +0500

    Merey Tursunbek-fix Singleton in TaskRepository

--- src/main/java/taskmanager/services/TaskService.java
+++ src/main/java/taskmanager/services/TaskService.java
@@ -1,9 +1,11 @@
-
 package taskmanager.services;
 
 import taskmanager.entities.Task;
 import taskmanager.repositories.TaskRepository;
+import taskmanager.services.notification.NotificationFactory;
+import taskmanager.services.notification.Notification;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskService {
     private final TaskRepository taskRepo;
@@ -16,15 +18,27 @@ public class TaskService {
         if (task.getDeadline().isBefore(java.time.LocalDate.now())) {
             throw new RuntimeException("Deadline cannot be in the past!");
         }
-        taskRepo.createTask(task);
+        taskRepo.save(task);
+
+        Notification messenger = NotificationFactory.getNotification("EMAIL");
+        if (messenger != null) {
+            messenger.send("Task '" + task.getTitle() + "' was successfully created!");
+        }
     }
 
+    public List<Task> getTasksByStatus(String status) {
+        List<Task> allTasks = taskRepo.findAll();
+
+        return allTasks.stream()
+                .filter(t -> t.getStatus().equalsIgnoreCase(status))
+                .collect(Collectors.toList());
+    }
 
     public Task getTaskById(int id) {
-        return taskRepo.findTaskById(id);
+        return taskRepo.findById(id);
     }
 
     public List<Task> getAllTasks() {
-        return taskRepo.listAllTasks();
+        return taskRepo.findAll();
     }
 }
